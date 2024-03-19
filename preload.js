320b7535ca968168cd46ac2bdd858be6a618b060
James S. Rossie <james@rossie.net>
2024-03-18 21:15:53 -0500

    More cursor hiding tweaks

--- preload.js
+++ preload.js
@@ -2,6 +2,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     // Set the background color to dark
     document.body.style.backgroundColor = '#232227';
+
     // Hide the cursor
-    document.body.style.cursor = 'none !important';
+    setInterval(() => {
+        document.body.style.cursor = 'none';
+    }, 1000); // Apply the style every 1000 milliseconds
 });
