75133c78d9736f5ab580770c5df424a4b3215470
James S. Rossie <james@rossie.net>
2025-01-05 14:52:43 -0600

    Try zoomfactor

--- app.js
+++ app.js
@@ -139,9 +139,7 @@ electronApp.on('ready', () => {
         }
     });
     mainWindow.setMenu(null); // Disable the default menu
-    mainWindow.webContents.executeJavaScript(`
-        document.body.style.zoom = window.devicePixelRatio;
-    `);
+    mainWindow.webContents.setZoomFactor(1.0); // Adjust this if needed
 
     // Create and display the initial view based on the default URL
     const initialView = createView(defaultUrl);
